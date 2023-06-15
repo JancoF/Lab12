3662ff28fb50d5247841ab2e062bde33a12d8ce7
Noe <noe.flores@tecsup.edu.pe>
2023-06-15 10:55:35 -0500

    actu

--- resources/views/home.php
+++ resources/views/home.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inovacion</title>
+    <title>Innovacion</title>
 </head>
 <body>
     
